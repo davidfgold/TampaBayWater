8564203f5b38acb2a8b73a22741a2b82b35fc3d8
degorelick <degorelick@gmail.com>
2022-02-21 22:36:24 -0500

    bug fixes in CIP schedule and plotting

--- FinancialModeling/TBW_financial_model_custom_output_plotting.py
+++ FinancialModeling/TBW_financial_model_custom_output_plotting.py
@@ -56,8 +56,8 @@ METRICS_VARIABLES = ['Fiscal Year', 'Debt Covenant Ratio', 'Rate Covenant Ratio'
 data_names_to_plot = ['Rate Covenant Ratio', 'Debt Covenant Ratio', 'Uniform Rate', 'Debt Service', 'Debt Service Deferred', 'Remaining Unallocated Deficit',
                       'Utility Reserve Fund Balance (Total)', 'Rate Stabilization Fund (Total)', 'CIP Fund (Total)', 'R&R Fund (Total)', 'Energy Savings Fund (Total)']
 formulation_to_plot = [125] # list all 
-simulation_to_plot = [0,1] # list all, IDs start at 0
-realization_to_plot = [5] # list which we want, IDs start at 1 not zero
+simulation_to_plot = [0,1,2,3,4,5,6,7,8] # list all, IDs start at 0
+realization_to_plot = [x for x in range(1,10)] # list which we want, IDs start at 1 not zero
 metrics_test_read = pd.read_csv(data_path + '/financial_metrics_f' + str(formulation_to_plot[0]) + '_s' + str(simulation_to_plot[0]) + '_r' + str(realization_to_plot[0]) + '.csv', index_col = 0)
 
 
@@ -82,6 +82,9 @@ for f in range(0,len(formulation_to_plot)):
         # cycle through realizations to collect data
         for r in range(0,len(realization_to_plot)):
             print('\t\tCollecting output for Realization ' + str(realization_to_plot[r]))
+            
+            if realization_to_plot[r] == 95:
+                continue
                 
             # read data input files
             actuals = pd.read_csv(data_path + '/budget_actuals_f' + str(formulation_to_plot[f]) + '_s' + str(simulation_to_plot[s]) + '_r' + str(realization_to_plot[r]) + '.csv', index_col = 0)
@@ -128,7 +131,7 @@ for f in range(0,len(formulation_to_plot)):
                 ax.set_xticklabels([int(x) for x in metrics['Fiscal Year'].values], rotation = 90)
                 
         # output and close figure to avoid overloading memory
-        plt.savefig(data_path + '/Custom_Outputs_Plot_f' + str(formulation_to_plot[f]) + '_s' + str(simulation_to_plot[s]) + ('_SINGLE_REALIZATION_r' + str(realization_to_plot[r])) if len(realization_to_plot) == 1 else None + '.png', bbox_inches= 'tight', dpi = 400)
+        plt.savefig(data_path + '/Custom_Outputs_Plot_f' + str(formulation_to_plot[f]) + '_s' + str(simulation_to_plot[s]) + (('_SINGLE_REALIZATION_r' + str(realization_to_plot[r])) if len(realization_to_plot) == 1 else '') + '.png', bbox_inches= 'tight', dpi = 400)
         plt.close()
                 
   
