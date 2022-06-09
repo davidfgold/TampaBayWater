faac271d5ce403d501102140679d7def14562fa1
cmpetagna <cmpetagna@gmail.com>
2022-06-09 17:56:37 -0400

    adds changes related to readingin historic records so that historic CIP records can be read in. Changes in placement of start_fy, end_fy, start_modeled_year included. Also includes changes to historic CIP records r script.

--- FinancialModeling/TBW_financial_model_old_CIP_records.py
+++ FinancialModeling/TBW_financial_model_old_CIP_records.py
@@ -12,8 +12,8 @@ CIP2020_2029 = pd.read_csv(hist_financial_path + '/FY2020-2029_CashFlowAnalysis_
 CIP2020_2029.drop(CIP2020_2029.columns[[0, 4, 5]], axis = 1, inplace = True)
 CIP2020_2029_melt = CIP2020_2029.melt(id_vars = ['Project No.', 'Project Name', 'Current Funding Source', 'Total Funds Needed'], value_vars = ['Remaining FY 2019', 'FY 2020', 'FY 2021', 'FY 2022', 'FY 2023', 'FY 2024', 'FY 2025', 'FY 2026', 'FY 2027', 'FY 2028', 'FY 2029', 'Future'], var_name = 'Year Spent', value_name = 'Dollars')
 CIP2020_2029_melt['Value'] = CIP2020_2029_melt['Dollars'].str.replace(',', '').str.replace('$', '').astype(float)
-CIP2020_2029_melt['Total Dollars']=CIP2020_2029['Total Funds Needed'].str.replace(',', '').str.replace('$', '').astype(float)
-CIP2020_2029_melt.drop(['Total Funds Needed'], axis = 1, inplace = True)
+CIP2020_2029_melt['Total Dollars']=CIP2020_2029_melt['Total Funds Needed'].str.replace(',', '').str.replace('$', '').astype(float)
+#CIP2020_2029_melt.drop(['Total Funds Needed'], axis = 1, inplace = True)
 
 ##Break down of small, medium, and large projects - using current FY cut-offs
 #Total_Capital_Needed = [(CIP2020_2029_melt['Total Dollars'] > 75000000), \
@@ -31,9 +31,19 @@ CIP2020_2029_melt['Total Capital Needed'] = np.select(Total_Capital_Needed, valu
 
 ##Now want to normalize the data
 ##Want to isolate each project, select just years where it is funded, re-index 0-x rather than 2019-2029
-for project in CIP2020_2029_melt.Project'Project Name']:
-    #project_data = CIP2020_2029_melt['Project Name']
-    first_year_funded = min()
+#CIP2020_2029_normalized = pd.Dataframe()
+projects = CIP2020_2029_melt['Project Name'].unique()
+project_data = {project : pd.DataFrame() for project in projects}
+for key in project_data.keys():
+    project_data[key] = CIP2020_2029_melt[:][CIP2020_2029_melt['Project Name'] == key]
+for project in project_data:
+    for year in project['Year Spent']:
+        if project['Project Value'] != 0:
+            first_year_funded = min(project['Year Spent'])
+
+    
+    #print(project_data)
+    #first_year_funded = min()
 
 import matplotlib
 import matplotlib.pyplot as plt
